01d3ddeb7ced13599bc4f6026a5a25aa5827d1af
AhmedAdel999 <es-Ahmed.Sultan2022@alexu.edu.eg>
2021-01-22 22:19:25 +0200

    Added user authentication

---
+++ Frontend/src/components/Communities.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Communities() {
+  return (
+    <div>
+      <h2>Communities</h2>
+    </div>
+  );
+}
+
+export default Communities;
---
+++ Frontend/src/redux/reducers/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux';
+import user from './user';
+import alert from './alert';
+
+const rootReducer = combineReducers({
+  user,
+  alert
+});
+
+export default rootReducer;
