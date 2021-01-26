c10c152af6718081dc088440ebce092dfd229b12
AhmedAdel999 <es-Ahmed.Sultan2022@alexu.edu.eg>
2021-01-26 12:33:47 +0200

    Added dashboard front end

--- Frontend/src/components/header/Header.js
+++ Frontend/src/components/header/Header.js
@@ -2,7 +2,6 @@ import { React, useState, useEffect } from 'react';
 import { FaBars, FaRegWindowClose } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 import { logOut } from '../../redux/actions/user';
-import './header.styles.scss';
 import { connect } from 'react-redux';
 
 const Header = ({ isAuthenticated, logOut }) => {
@@ -14,33 +13,46 @@ const Header = ({ isAuthenticated, logOut }) => {
   const closeHamburgerMenu = () => {
     setOpen(false);
   };
-  
+
   return (
-    <header className="header">
-      <div className="brand">
-        <a href="#">Reddit Clone</a>
+    <header className='header'>
+      <div className='brand'>
+        <a href='#'>Reddit Clone</a>
       </div>
-      <FaBars onClick={openHamburgerMenu} className="hamburger-icon"/>
+      <FaBars onClick={openHamburgerMenu} className='hamburger-icon' />
       <ul className={`nav-menu ${open ? ' open' : ''}`}>
-        <FaRegWindowClose onClick={closeHamburgerMenu} className={`hamburger-close ${open ? ' open' : ''}`}/>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/communities">Communities</Link></li>
-        {
-          isAuthenticated ?
-            <li><Link to="#!" onClick={logOut}>Log Out</Link></li>
-           :
-            <>
-              <li><Link to="/signup">Sign Up</Link></li>
-              <li><Link to="/login">Log In</Link></li>
-            </>
-          
-        }
+        <FaRegWindowClose
+          onClick={closeHamburgerMenu}
+          className={`hamburger-close ${open ? ' open' : ''}`}
+        />
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <Link to='/communities'>Communities</Link>
+        </li>
+        {isAuthenticated ? (
+          <li>
+            <Link to='#!' onClick={logOut}>
+              Log Out
+            </Link>
+          </li>
+        ) : (
+          <>
+            <li>
+              <Link to='/signup'>Sign Up</Link>
+            </li>
+            <li>
+              <Link to='/login'>Login</Link>
+            </li>
+          </>
+        )}
       </ul>
     </header>
   );
 };
 
-const mapStateToProps = state => ({
-  isAuthenticated: state.user.isAuthenticated
+const mapStateToProps = (state) => ({
+  isAuthenticated: state.user.isAuthenticated,
 });
-export default connect(mapStateToProps, {logOut})(Header);
+export default connect(mapStateToProps, { logOut })(Header);
